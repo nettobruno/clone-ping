44243e9554db71b5fcbb51bb26f645fa6437e4fe
nettobruno <brunonettomac@outlook.com>
2020-07-09 20:13:45 -0300

    Improved email verification logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-function validacaoEmail(field) {
-  usuario = field.value.substring(0, field.value.indexOf("@"));
-  dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
-  
-  if ((usuario.length >=1) ||
-      (dominio.length >=3) || 
-      (usuario.search("@")==-1) || 
-      (dominio.search("@")==-1) ||
-      (usuario.search(" ")==-1) || 
-      (dominio.search(" ")==-1) ||
-      (dominio.search(".")!=-1) ||      
-      (dominio.indexOf(".") >=1)|| 
-      (dominio.lastIndexOf(".") < dominio.length - 1)) {
-        let textError = document.getElementById("msgemail");
-        textError.style.display = "block";
-        textError.style.color = "hsl(354, 100%, 66%)";
-    
-        let element = document.getElementById("email");
-        element.style.border = "1px solid hsl(354, 100%, 66%)";
+let inputEmail = document.querySelector('#email');
+let textError = document.querySelector("form p");
+let buttonForm = document.querySelector('#formButton');
+
+buttonForm.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  var regex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+  if(!regex.test(inputEmail.value)) {
+    inputEmail.classList.add('borderRed');
+    textError.style.color = 'hsl(354, 100%, 66%)';
+    textError.classList.add('showError');
   }
-  else{}
-}
+  else {
+    inputEmail.classList.remove('borderRed');
+    textError.classList.remove('showError');
+    alert('Email válido');
+  }
+})
